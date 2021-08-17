266acead29014d60251b957e0886f6ab79a20e64
mjbiener <michaelbiener@yahoo.com>
2021-08-17 19:47:44 -0400

    minor model updates all setup links work across views/pages -done for night

--- books/models.py
+++ books/models.py
@@ -50,5 +50,8 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(blank=True)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return f'{self.name}'
